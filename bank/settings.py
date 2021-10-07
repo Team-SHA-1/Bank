b956a33da6ea51f5426034007bd9c43be1513966
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2021-10-07 10:14:30 +0530

    Update settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = '*9!ds4+q7g_n%wk59ppebdn^s044(^%#msb4ssni%jcz5=k%^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bank-for-canfirm.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['bank-for-canfirm.herokuapp.com']
 
 
 # Application definition
